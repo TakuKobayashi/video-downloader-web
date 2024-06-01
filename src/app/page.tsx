cd57cced47a67f8d32c2abfbaf0ef8ce5b9cf323
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2024-06-02 03:03:04 +0900

    install and setup prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import Image from "next/image";
+import Image from 'next/image';
 import { Button } from '@chakra-ui/react';
 
 export default function Home() {
   return (
     <main>
       <div>
-        <Button colorScheme="blue">Click Me</Button>
+        <Button colorScheme='blue'>Click Me</Button>
       </div>
     </main>
   );
