1232175e64cc46a459a3fdaea9a451256265b3bb
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2024-06-02 18:15:37 +0900

    downloader test

--- functions/api/downloader.ts
+++ functions/api/downloader.ts
@@ -1,3 +1,6 @@
-export function onRequestGet(context) {
-    return Response.json({...context, hello: 'world'})
+export async function onRequestGet(context) {
+  const response = await context.next();
+  response.headers.set('Access-Control-Allow-Origin', '*');
+  response.headers.set('Access-Control-Max-Age', '86400');
+  return response.json({ hello: 'world' });
 }
