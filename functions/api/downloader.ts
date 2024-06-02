0f1db531f055eac58228c9a13584a2eb4f2cf3c5
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2024-06-02 18:27:09 +0900

    fix response to json

--- functions/api/downloader.ts
+++ functions/api/downloader.ts
@@ -2,5 +2,6 @@ export async function onRequestGet(context) {
   const response = await context.next();
   response.headers.set('Access-Control-Allow-Origin', '*');
   response.headers.set('Access-Control-Max-Age', '86400');
-  return response.json({ hello: 'world' });
+  response.json(context.data);
+  return response
 }
