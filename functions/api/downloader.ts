7cf96bbb3fc3096db446180dbce9ffd7bf8a6626
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2024-06-03 01:16:56 +0900

    formに入力された内容を受け取って精査する処理の作成

--- functions/api/downloader.ts
+++ functions/api/downloader.ts
@@ -1,7 +1,48 @@
-export async function onRequestGet(context) {
-  const response = await context.next();
-  response.headers.set('Access-Control-Allow-Origin', '*');
-  response.headers.set('Access-Control-Max-Age', '86400');
-  response.json(context.data);
-  return response
+export async function onRequest(context) {
+  const responseData = await readRequestBody(context.request);
+  const responseHeaders = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': '*',
+    'Access-Control-Max-Age': '86400',
+  };
+  if (typeof responseData === 'string') {
+    return new Response(responseData, {
+      headers: responseHeaders,
+    });
+  } else if (typeof responseData === 'object') {
+    return Response.json(
+      { hello: 'object' },
+      {
+        headers: responseHeaders,
+      },
+    );
+  }
+  return Response.json(
+    {},
+    {
+      headers: responseHeaders,
+    },
+  );
+}
+
+async function readRequestBody(request) {
+  const contentType = request.headers.get('content-type');
+  if (contentType.includes('application/json')) {
+    return await request.json();
+  } else if (contentType.includes('application/text')) {
+    return request.text();
+  } else if (contentType.includes('text/html')) {
+    return request.text();
+  } else if (contentType.includes('form')) {
+    const formData = await request.formData();
+    const body = {};
+    for (const entry of formData.entries()) {
+      body[entry[0]] = entry[1];
+    }
+    return body;
+  } else {
+    // Perhaps some other type of data was submitted in the form
+    // like an image, or some other binary data.
+    return 'a file';
+  }
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,28 @@
+'use client';
+
+import { useState } from 'react';
 import { Button, Input } from '@chakra-ui/react';
+import axios from 'axios';
 
 export default function Home() {
+  const [inputUrl, setInputUrl] = useState('');
+  const onLoadVideoInfo = async (url: string) => {
+    const form = new FormData();
+    form.append('url', url);
+    const response = await axios.post('/api/downloader', form);
+    console.log(response.data);
+  };
   return (
     <main>
       <div>
-        <Input variant='outline' placeholder='youtube url' />
-        <Button colorScheme='blue'>Download</Button>
+        <Input
+          variant='outline'
+          placeholder='youtube url'
+          onChange={(e) => setInputUrl(e.target.value)}
+        />
+        <Button colorScheme='blue' onClick={() => onLoadVideoInfo(inputUrl)}>
+          Download
+        </Button>
       </div>
     </main>
   );
